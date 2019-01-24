3bcd59434016d04670da56c79050794fa666d46e
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2019-01-23 22:09:10 -0500

    Add new components

---
+++ src/components/ConsoleCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <h2>{{ heading }}</h2>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  background-color: white;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 3px;
+  padding: 5px;
+}
+
+.card:hover {
+  box-shadow: 0 10px 18px 0 rgba(0, 0, 0, 0.2);
+}
+
+h2 {
+  margin: 5px;
+  margin-left: 0.7em;
+  font-size: 14px;
+  line-height: 20px;
+}
+</style>
---
+++ src/components/ConsoleLegend.vue
@@ -0,0 +1,84 @@
+<template>
+  <console-card heading="MAP LEGEND">
+    <div class="legend_row" v-for="legend in legends" :key="legend.id">
+      <span
+        :style="{ display: legend.displayed ? 'block' : 'none' }"
+        v-html="legend.html"
+      ></span>
+    </div>
+  </console-card>
+</template>
+
+<script>
+import { eventBus } from "../main";
+import ConsoleCard from "./ConsoleCard.vue";
+
+export default {
+  components: { ConsoleCard },
+  data() {
+    return {
+      legends: [
+        {
+          id: 1,
+          html: `<div class="label" id="sensor"></div>
+      <div class="label">Sea-level sensors</div>`,
+          displayed: true
+        },
+        {
+          id: 2,
+          html: `<div class="colors"></div>
+      <div style="overflow: hidden;">
+        <div class="label third">5ft</div>
+        <div class="label third">10ft</div>
+        <div class="label third">15ft</div>
+      </div>`,
+          displayed: false
+        }
+      ]
+    };
+  },
+  created() {
+    eventBus.$on("update-legend", emitterId => {
+      this.legends.forEach(legend => {
+        legend.displayed = legend.id === emitterId || legend.id === 1; // always display sensor legend
+      });
+    });
+  }
+};
+</script>
+
+<style scoped>
+.legend_row {
+  margin: 5px;
+}
+
+.legend_row >>> .label {
+  display: inline-block;
+  text-align: center;
+  font-size: 14px;
+}
+
+.legend_row >>> .third {
+  width: 32%;
+}
+
+.legend_row >>> #sensor {
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #007cbf;
+  opacity: 0.8;
+  margin-right: 6px;
+}
+
+.legend_row >>> .colors {
+  height: 12px;
+  background: linear-gradient(
+    to right,
+    turquoise,
+    deepskyblue,
+    dodgerblue,
+    navy
+  );
+}
+</style>
