6e397b47b642ee1dadbd6cee32626705497a002b
Sandra <salsayar3@gatech.edu>
2019-02-09 17:05:49 -0500

    Added the hover over functionality to sensors

--- src/App.vue
+++ src/App.vue
@@ -88,33 +88,37 @@ export default {
         });
 
 
-        // When a click event occurs on a feature in the places layer, open a popup at the
-        // location of the feature, with description HTML from its properties.
-        map.on('click', 'point', function (e) {
-          var coordinates = e.features[0].geometry.coordinates.slice();
-          var description = e.features[0].properties.description;
-           
-          // Ensure that if the map is zoomed out such that multiple copies of the feature
-          // are visible, the popup appears over the copy being pointed to.
-          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-          }
-           
-          new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(description)
-            .addTo(map);
-        });
-           
-        // Change the cursor to a pointer when the mouse is over the places layer.
-        map.on('mouseenter', 'point', function () {
-          map.getCanvas().style.cursor = 'pointer';
-        });
-           
-        // Change it back to a pointer when it leaves.
-        map.on('mouseleave', 'point', function () {
-          map.getCanvas().style.cursor = '';
-        });
+      // Create a popup, but don't add it to the map yet.
+      var popup = new mapboxgl.Popup({
+        closeButton: false,
+        closeOnClick: false
+      });
+       
+      map.on('mouseenter', 'point', function(e) {
+      // Change the cursor style as a UI indicator.
+        map.getCanvas().style.cursor = 'pointer';
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
+         
+        // Ensure that if the map is zoomed out such that multiple copies of the feature are visible,
+        // the popup appears over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+       
+      // Populate the popup and set its coordinates.
+        popup.setLngLat(coordinates)
+        // Need to be replaced with sensor location
+        .setHTML("<h3>Sensor</h3>" +
+          "<body>Reading: <br></body>" +
+          "<body>Time: </body>")
+        .addTo(map);
+      });
+       
+      map.on('mouseleave', 'point', function() {
+        map.getCanvas().style.cursor = '';
+        popup.remove();
+      });
     }
   }
 };
@@ -138,6 +142,9 @@ export default {
 .mapboxgl-popup {
   max-width: 400px;
   font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+}
+.mapboxgl-popup-tip {
+  border: 0px;
 }
 
 /* Override default CSS for search box */
