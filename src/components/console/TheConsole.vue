71c348f88fad5fe2303dc1ac9d2e81f43122e744
rconklin6 <russellconklinbusiness@gmail.com>
2019-02-25 22:04:56 -0500

    Adding new subfolders and pushing to origin

---
+++ src/components/console/TheConsole.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="console">
+    <div style="height: 48px;"></div>
+    <ConsoleLayersList />
+    <div style="height: 6px;"></div>
+    <ConsoleLegend />
+    <div style="height: 6px;"></div>
+    <console-card v-if="infoCardVisible" heading="SENSOR INFORMATION">
+    </console-card>
+  </div>
+</template>
+
+<script>
+import ConsoleLayersList from "./ConsoleLayersList";
+import ConsoleLegend from "./ConsoleLegend";
+import ConsoleCard from "./ConsoleCard";
+import { eventBus } from "../../main";
+
+export default {
+  components: {
+    ConsoleLayersList,
+    ConsoleLegend,
+    ConsoleCard
+  },
+  data() {
+    return {
+      infoCardVisible: false
+    };
+  },
+  created() {
+    eventBus.$on("sensor-clicked", (selected, _) => {
+      this.infoCardVisible = selected;
+    });
+  }
+};
+</script>
+
+<style scoped>
+#console {
+  position: absolute;
+  width: 286px;
+  margin: 10px;
+  padding: 8px 8px;
+  background-color: rgb(225, 225, 225);
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  border-radius: 3px;
+  z-index: 0;
+}
+</style>
---
+++ src/components/timelapse/TimelapseBar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id = "bar">
+    <v-slider
+      v-model="sliderVal"
+      :tick-labels="ticksLabels"
+      :max="12"
+      step="1"
+      ticks="always"
+      tick-size="3"
+      color='teal'
+      always-dirty
+    ></v-slider>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../../main";
+export default {
+  data () {
+    return {
+      sliderVal: 0,
+      ticksLabels: [
+        '12 am',
+        '2 am',
+        '4 am',
+        '6 am',
+        '8 am',
+        '10 am',
+        '12 pm',
+        '2 pm',
+        '4 pm',
+        '6 pm',
+        '8 pm',
+        '10 pm',
+        '12 am'
+      ],
+      interval: null
+    }
+  },
+  methods: {
+    advanceTimelapse () {
+      if (this.sliderVal == 12) {
+        this.sliderVal = 0
+      } else {
+        this.sliderVal++
+      }
+    }
+  },
+  watch: {
+    sliderVal: function() {
+      eventBus.$emit("timelapse-pulse", this.sliderVal)
+    }
+  },
+  created() {
+    eventBus.$on("toggle-timelapse", (isPlaying) => {
+      if(isPlaying) {
+        this.interval = setInterval(this.advanceTimelapse, 1000)
+      } else {
+        clearInterval(this.interval)
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+#bar {
+  position: fixed;
+  bottom: 10px;
+  right: 100px;
+  width: 1000px;
+  margin: 10px;
+  padding: 8px 8px;
+  border-radius: 3px;
+  z-index: 0;
+}
+</style>
