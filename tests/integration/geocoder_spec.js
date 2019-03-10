d8a86daf7a8fe6a8342dd39545c2081ba11b1c75
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2019-03-10 03:36:41 -0400

    Try getting cypress to work

--- tests/integration/geocoder_spec.js
+++ tests/integration/geocoder_spec.js
@@ -1,8 +1,18 @@
-describe('The geocoder', function() {
-  it('supports searching for a sensor', function() {
-  	cy.visit("/")
+describe("The geocoder", function() {
+  it("supports searching for a sensor", function() {
+    cy.server();
+    cy.route("GET", "https://api.mapbox.com/styles/**").as("getMap");
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        delete win.fetch;
+      }
+    });
+    
+    cy.wait("@getMap", {timeout: 35000});
 
-    cy.get('input[placeholder="Search"]').type('sensor')
-    cy.get('ul.suggestions li').first().click()
-  })
-})
+    cy.get('input[placeholder="Search"]').type("sensor");
+    cy.get("ul.suggestions li")
+      .first()
+      .click();
+  });
+});
--- tests/integration/interactive_map_spec.js
+++ tests/integration/interactive_map_spec.js
@@ -1,16 +1,25 @@
-describe('The web app', function() {
-  it('successfully loads', function() {
-    cy.visit('/')
-  })
+describe("The web app", function() {
+  it("successfully loads", function() {
+    cy.visit("/");
+  });
 
-  it('has a basic map with interactive controls', function () {
-    cy.visit('/')
+  it("has a basic map with interactive controls", function() {
+    cy.server();
+    cy.route("GET", "https://api.mapbox.com/styles/**").as("getMap");
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        delete win.fetch;
+      }
+    });
+    cy.get("#app");
+    cy.get("#map"); // map element
     
-  	cy.get('#map') // map element
-  	cy.get('[aria-label="Geolocate"]') // geolocate
-  	cy.get('[aria-label="Zoom in"]').click() // zoom in button
-  	cy.get('[aria-label="Zoom out"]').click() // zoom out button
-  	cy.get('[aria-label="Reset bearing to north"]').click() // bearing button
-  	cy.get('input[placeholder="Search"]')
-  })
-})
+    cy.wait("@getMap", {timeout: 35000});
+
+    cy.get('[aria-label="Geolocate"]'); // geolocate
+    cy.get('[aria-label="Zoom in"]').click(); // zoom in button
+    cy.get('[aria-label="Zoom out"]').click(); // zoom out button
+    cy.get('[aria-label="Reset bearing to north"]').click(); // bearing button
+    cy.get('input[placeholder="Search"]');
+  });
+});
