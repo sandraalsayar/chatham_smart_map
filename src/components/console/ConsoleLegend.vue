c1abb44ccea8e72b0d442e85a7dcb6639621e726
Sandra <salsayar3@gatech.edu>
2019-03-29 11:34:19 -0400

    UI Change
    Changed Sea Level to Water Level

--- src/components/console/ConsoleLegend.vue
+++ src/components/console/ConsoleLegend.vue
@@ -21,7 +21,7 @@ export default {
         {
           id: 1,
           html: `<div class="label" id="sensor"></div>
-      <p class="label" style="font-size:16px">Sea level sensors</p>`,
+      <p class="label" style="font-size:16px">Water level sensors</p>`,
           displayed: true
         },
         {
