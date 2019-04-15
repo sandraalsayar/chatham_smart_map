32c0925309af93853e32c221b052c26f98cf0d8c
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2019-04-14 21:25:24 -0400

    Get started with graphs

---
+++ src/components/console/LineChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <highcharts :options="chartOptions"></highcharts>
+</template>
+
+<script>
+import { Chart } from "highcharts-vue";
+
+export default {
+  components: {
+    highcharts: Chart
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  computed: {
+    chartOptions() {
+      return {
+        title: {
+          text: `${this.title} Data`
+        },
+        xAxis: {
+          type: "datetime"
+        },
+        yAxis: {
+          title: {
+            text: this.title
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        series: [
+          {
+            name: "USD to EUR",
+            data: this.data,
+            color: this.color
+          }
+        ]
+      };
+    }
+  }
+};
+</script>
